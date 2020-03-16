d1fc4e4e2ede13be308585c7a624ede9692c9eef
demian2435 <demian2435@gmail.com>
2020-03-16 21:49:54 +0100

    Lezione 03 terminata

--- src/main/java/it/polito/tdp/esempioGit/FXMLController.java
+++ src/main/java/it/polito/tdp/esempioGit/FXMLController.java
@@ -4,22 +4,71 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class FXMLController implements Initializable {
-    
+public class FXMLController {
+
     @FXML
-    private Label label;
-    
+    private ResourceBundle resources;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private URL location;
+
+    @FXML
+    private TextField txt_nome;
+
+    @FXML
+    private TextField txt_eta;
+
+    @FXML
+    private Button btn_ok;
+
+    @FXML
+    private Label lbl_risposta;
+
+    @FXML
+    void handle_btn_ok(ActionEvent event) {
+    	
+    	// CONTROLLO DATI INSERITI
+    	String nome = txt_nome.getText();
+    	if(nome.length() == 0) {
+    		lbl_risposta.setText("Inserisci un nome!");
+    		return;
+    	}
+    	String etaString = txt_eta.getText();
+    	if(nome.length() == 0) {
+    		lbl_risposta.setText("Inserisci l'età!");
+    		return;
+    	}
+    	
+    	int eta;
+    	try {
+    		eta = Integer.parseInt(etaString);
+    	} catch(Exception e) {
+    		lbl_risposta.setText("Inserisci un'età valida!");
+    		return;
+    	}
+    	
+    	// ALGORITMO DI SCELTA SALUTO
+    	String saluto;
+    	if(eta < 18) {  		
+    		saluto = "Ciao " + nome + "!";	
+    	} else {		
+    		saluto = "Buongiorno " + nome + "!";  		
+    	}
+    	
+    	// AGGIONRAMENTO LABEL
+    	lbl_risposta.setText(saluto);
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+    @FXML
+    void initialize() {
+        assert txt_nome != null : "fx:id=\"txt_nome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txt_eta != null : "fx:id=\"txt_eta\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btn_ok != null : "fx:id=\"btn_ok\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert lbl_risposta != null : "fx:id=\"lbl_risposta\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
 }
